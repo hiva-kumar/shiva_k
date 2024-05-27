05534d6c68be7ef779297f20644b46034bcdc8e0
hiva-kumar <147628186+hiva-kumar@users.noreply.github.com>
2024-05-27 13:18:09 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+//import React, { useontext } from "react";
+//import { useGlobalcontext } from "./context";
+
+import Movies from "./Movies";
+import Search from "./Search";
+
+const Home = () => {
+    return (
+        <>
+            <Search />
+            <Movies />
+        </>
+    );
+};
+
+
+export default Home;
